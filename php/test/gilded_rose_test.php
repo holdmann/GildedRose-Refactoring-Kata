9f70f98873f3aa0382f22e165326a15d163fd06a
Sherbakov Oleg <holdmann@yandex.ru>
2016-06-12 00:55:07 +0400

    Added default item unit test.

--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -4,11 +4,38 @@ require_once 'gilded_rose.php';
 
 class GildedRoseTest extends PHPUnit_Framework_TestCase {
 
-    function testFoo() {
-        $items = array(new Item("foo", 0, 0));
-        $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
-        $this->assertEquals("fixme", $items[0]->name);
+    /**
+     * @var string Folder that contents fixtures data.
+     */
+    protected $fixturesFolder = 'fixtures';
+
+    /**
+     * Testing of Default item.
+     */
+    function testDefaultItem()
+    {
+        $fixturesFilename = file_get_contents(__DIR__ . '/' . $this->fixturesFolder . '/default-item.json');
+        $fixtures = json_decode($fixturesFilename);
+
+        foreach($fixtures as $id=>$fixture) {
+            $items = [
+                new Item("Any default item", $fixture->properties->sellIn, $fixture->properties->quality)
+            ];
+            $gildedRose = new GildedRose($items);
+
+            for($i = 0; $i<count($fixture->items); $i++) {
+                $this->assertEquals(
+                    $fixture->items[$i]->sellIn, 
+                    $items[0]->sellIn
+                );
+
+                $this->assertEquals(
+                    $fixture->items[$i]->quality,
+                    $items[0]->quality
+                );
+                $gildedRose->updateQuality();
+            }
+        }
     }
 
 }
