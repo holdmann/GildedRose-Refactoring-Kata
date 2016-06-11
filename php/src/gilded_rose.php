cc9496892295a45f1cdf303d16e0c0aaf1585777
Sherbakov Oleg <holdmann@yandex.ru>
2016-06-12 01:10:45 +0400

    Replaced old logic with new one.

--- php/src/gilded_rose.php
+++ php/src/gilded_rose.php
@@ -10,109 +10,59 @@ class GildedRose {
 
     function updateQuality() {
         foreach ($this->items as $item) {
-            if (in_array($item->name, ['Aged Brie', 'Sulfuras, Hand of Ragnaros', 'Backstage passes to a TAFKAL80ETC concert'])) {
-                switch ($item->name) {
-                    case 'Aged Brie':
-                        $qualityIncreaseBy = $item->sellIn > 0 ? 1 : 2;
+            switch ($item->name) {
+                case 'Aged Brie':
+                    $qualityIncreaseBy = $item->sellIn > 0 ? 1 : 2;
 
-                        $item->sellIn -= 1;
+                    $item->sellIn -= 1;
 
-                        if ($item->sellIn < 0)
-                            $qualityIncreaseBy = 2;
+                    if ($item->sellIn < 0)
+                        $qualityIncreaseBy = 2;
 
-                        $item->quality += $qualityIncreaseBy;
+                    $item->quality += $qualityIncreaseBy;
 
-                        if ($item->quality > 50)
-                            $item->quality = 50;
+                    if ($item->quality > 50)
+                        $item->quality = 50;
 
-                        break;
+                    break;
 
-                    case 'Sulfuras, Hand of Ragnaros':
-                        // legendary item, do nothing.
-                        break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    // legendary item, do nothing.
+                    break;
 
-                    case 'Backstage passes to a TAFKAL80ETC concert':
-                        $qualityIncreaseBy = 1;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $qualityIncreaseBy = 1;
 
-                        if ($item->sellIn <= 10)
-                            $qualityIncreaseBy = 2;
-                        if ($item->sellIn <= 5)
-                            $qualityIncreaseBy = 3;
+                    if ($item->sellIn <= 10)
+                        $qualityIncreaseBy = 2;
+                    if ($item->sellIn <= 5)
+                        $qualityIncreaseBy = 3;
 
-                        $item->sellIn -= 1;
+                    $item->sellIn -= 1;
 
-                        $item->quality += $qualityIncreaseBy;
+                    $item->quality += $qualityIncreaseBy;
 
-                        if ($item->sellIn < 0)
-                            $item->quality = 0;
+                    if ($item->sellIn < 0)
+                        $item->quality = 0;
 
-                        if ($item->quality > 50)
-                            $item->quality = 50;
-                        break;
+                    if ($item->quality > 50)
+                        $item->quality = 50;
+                    break;
 
-                    default:
-                        $qualityDecreaseBy = $item->sellIn > 0 ? 1 : 2;
+                default:
+                    $qualityDecreaseBy = $item->sellIn > 0 ? 1 : 2;
 
-                        $item->sellIn -= 1;
+                    $item->sellIn -= 1;
 
-                        if ($item->sellIn < 0)
-                            $qualityDecreaseBy = 2;
+                    if ($item->sellIn < 0)
+                        $qualityDecreaseBy = 2;
 
-                        $item->quality -= $qualityDecreaseBy;
+                    $item->quality -= $qualityDecreaseBy;
 
-                        if ($item->quality < 0)
-                            $item->quality = 0;
-                        
-                        break;
-                }
-                // terminate method execution.
-                return;
-            }
+                    if ($item->quality < 0)
+                        $item->quality = 0;
 
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-            
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+                    break;
             }
         }
     }
