ae4c72c35fb5262aa8cc1fee7779582f00d41127
Sherbakov Oleg <holdmann@yandex.ru>
2016-06-12 19:24:47 +0400

    Removed "require" secton from tests to common class, that includes everywhere.

--- php/src/gilded_rose.php
+++ php/src/gilded_rose.php
@@ -1,5 +1,16 @@
 <?php
 
+require_once 'Item.php';
+require_once 'common/ItemFactory.php';
+require_once 'common/BaseItem.php';
+require_once 'exception/GildedRoseException.php';
+require_once 'exception/ItemQualityOutOfRangeException.php';
+require_once 'items/AgedBrie.php';
+require_once 'items/Sulfuras.php';
+require_once 'items/BackstagePass.php';
+require_once 'items/ConjuredManaCake.php';
+require_once 'items/DefaultItem.php';
+
 class GildedRose {
 
     private $items;
--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -1,16 +1,6 @@
 <?php
 
 require_once 'gilded_rose.php';
-require_once 'Item.php';
-require_once 'common/ItemFactory.php';
-require_once 'common/BaseItem.php';
-require_once 'exception/GildedRoseException.php';
-require_once 'exception/ItemQualityOutOfRangeException.php';
-require_once 'items/AgedBrie.php';
-require_once 'items/Sulfuras.php';
-require_once 'items/BackstagePass.php';
-require_once 'items/ConjuredManaCake.php';
-require_once 'items/DefaultItem.php';
 
 class GildedRoseTest extends PHPUnit_Framework_TestCase {
 
